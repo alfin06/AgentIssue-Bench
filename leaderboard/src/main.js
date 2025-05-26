929d2ad59cd8b159642c8ba1c85c1978a4723266
Alfin Rahardja <alfin06@users.noreply.github.com>
2025-05-26 14:25:37 +0800

    Remove leaderboard

--- leaderboard/src/main.js
+++
@@ -1,8 +0,0 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router' // ⬅️ Import the router
-import 'bootstrap/dist/css/bootstrap.min.css'
-
-createApp(App).use(router).mount('#app')

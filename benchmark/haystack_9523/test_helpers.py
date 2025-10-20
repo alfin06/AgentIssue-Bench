f3eca4dbe51017671c7bde951857cc197a2454db
Alfin Rahardja <alfin06@users.noreply.github.com>
2025-10-20 15:52:46 +0800

    Remove folder temporarily

--- benchmark/haystack_9523/test_helpers.py
+++
@@ -1,7 +0,0 @@
-def doc_to_string(documents) -> str:
-    return "documents processed"
-
-class MockChatGenerator:
-    def run(self, messages, tools=None, **kwargs):
-        from haystack.dataclasses import ChatMessage
-        return {"replies": [ChatMessage.from_assistant("Mocked final answer.")]}
